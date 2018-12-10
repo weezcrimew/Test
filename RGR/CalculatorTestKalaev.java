e72610fc3188954854a0fabe43eadf43a40224f2
SomeSomeRaw <36765400+SomeSomeRaw@users.noreply.github.com>
2018-12-10 17:16:55 +0500

    Add files via upload

---
+++ RGR/CalculatorTestKalaev.java
@@ -0,0 +1,10 @@
+package RGR;
+
+public class CalculatorTestKalaev {
+
+	public static void main(String[] args) throws InterruptedException  {
+		String[] a = {"4", "5", "6"}; //Создание входных данных
+		CalculatorApp.main(a); //Запуск программы, которую необходимо протестировать, с заданными входными данными
+	}
+
+}
---
+++ RGR/CalculatorTestSalimov.java
@@ -0,0 +1,10 @@
+package RGR;
+
+public class CalculatorTestSalimov {
+
+	public static void main(String[] args) throws InterruptedException  {
+		String[] a = {"7", "8", "9"}; //Создание входных данных
+		CalculatorApp.main(a); //Запуск программы, которую необходимо протестировать, с заданными входными данными
+	}
+
+}
