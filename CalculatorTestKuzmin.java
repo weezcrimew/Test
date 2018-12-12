493423770422f7addf7e7b720c14ea6259602553
SomeSomeRaw <36765400+SomeSomeRaw@users.noreply.github.com>
2018-12-12 16:35:46 +0500

    Add files via upload

---
+++ CalculatorTestKuzmin.java
@@ -0,0 +1,10 @@
+package RGR;
+
+public class CalculatorTestKuzmin {
+
+	public static void main(String[] args) throws InterruptedException  {
+		String[] a = {"10", "11", "12"}; //Создание входных данных
+		CalculatorApp.main(a); //Запуск программы, которую необходимо протестировать, с заданными входными данными
+	}
+
+}
