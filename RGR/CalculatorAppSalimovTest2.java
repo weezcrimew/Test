f21408c8d142dcffb54cdaedb31d805422a0be0e
weezcrimew <45969392+weezcrimew@users.noreply.github.com>
2018-12-18 22:46:17 +0500

    Update CalculatorAppSalimovTest2.java

--- RGR/CalculatorAppSalimovTest2.java
+++ RGR/CalculatorAppSalimovTest2.java
@@ -9,11 +9,11 @@ public class CalculatorAppSalimovTest2 {
 	@Test
 	public void test() throws InterruptedException {
 		Calculator c = new Calculator(); //create instance of the calculator
-		c.textField1.setText("4"); //set test in the text field
-		c.textField2.setText("5"); //set test in the text field
-		c.textField3.setText("6"); //set test in the text field
+		c.textField1.setText("22"); //set test in the text field
+		c.textField2.setText("23"); //set test in the text field
+		c.textField3.setText("24"); //set test in the text field
 		c.countB.doClick(); //do click on the button
-		assertEquals("2.", c.textField4.getText()); //check results
+		assertEquals("1", c.textField4.getText()); //check results
 		
 	}
 
